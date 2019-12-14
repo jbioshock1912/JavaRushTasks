57b0743428598d205bf5be95fe3d423bdc8fdae2
jbioshock1912 <loh7457490>
2019-12-14 18:06:13 +0300

    tips

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -1,28 +1,28 @@
 package com.javarush.task.task05.task0502;
 
-
+// класс CCat с переменными класса возраст, веси, сила
 public class Cat {
     public int age;
     public int weight;
     public int strength;
-
+// метод класса Cat.  Метода вне класса не бывает!!!
     public int getAge() {
-        return this.age;
+        return this.age;// возвращает переменную инт
     }
-
+// конструктор без параметров
     public Cat() {
     System.out.println("Cat created without variables!");
     }
-
+    // конструктор с параметрами. конструктор называется как класс, но  нет и не может быь возвращаемого значения
     public Cat(int years, int power) {
         this.age = years;
         this.strength = power;
         System.out.println("Cat created with variables!");
     }
-
+// метод с одним параметром anotherCat типа Cat. Всё что внутри это переменные внутри метода. Переменные этого метода, не класса
     public boolean fight(Cat anotherCat) {
         int points = 0;
-        if (anotherCat.age < this.age) {
+        if (anotherCat.age < this.age) { //вызыываем переменные класса
             points++;
         }
         if (anotherCat.weight < this.weight) {
@@ -36,7 +36,7 @@ public class Cat {
         }
         return false;
     }
-
+// void это означает что нт вовращаемого значения
     public static void main(String[] args) {
         Cat snow = new Cat();
         Cat rain = new Cat(5, 4);
